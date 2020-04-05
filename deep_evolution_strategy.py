e9f5e12a847c7ddd1e5f1cb8a74c3d11c489d780
cuevas1208 <cuevas1208@gmail.com>
2020-04-05 00:51:36 -0700

    first commit

---
+++ deep_evolution_strategy.py
@@ -0,0 +1,225 @@
+import pickle
+import time
+import os
+
+import matplotlib.pyplot as plt
+import numpy as np
+
+def load_model(self, filename):
+    with open(filename, 'rb') as input:
+        model = pickle.load(input)
+    return model
+
+
+# return the t to n closing price
+def get_state(data, t, n):
+    d = t - n + 1
+    block = data[d: t + 1] if d >= 0 else -d * [data[0]] + data[: t + 1]
+    res = []
+    for i in range(n - 1):
+        res.append(block[i + 1] - block[i])
+    return np.array([res])
+
+
+class Model:
+    def __init__(self, input_size, layer_size, output_size):
+        self.input_size = input_size
+        self.layer_size = layer_size
+        self.output_size = output_size
+        self.weights = [
+            np.random.randn(input_size, layer_size),
+            np.random.randn(layer_size, output_size),
+            np.random.randn(layer_size, 1),
+            np.random.randn(1, layer_size),
+        ]
+
+    def predict(self, inputs):
+        feed = np.dot(inputs, self.weights[0]) + self.weights[-1]
+        decision = np.dot(feed, self.weights[1])
+        buy = np.dot(feed, self.weights[2])
+        return decision, buy
+
+    def get_weights(self):
+        return self.weights
+
+    def set_weights(self, weights):
+        self.weights = weights
+
+
+class Deep_Evolution_Strategy:
+    def __init__(
+            self, weights, reward_function, population_size, sigma, learning_rate
+    ):
+        self.weights = weights
+        self.reward_function = reward_function
+        self.population_size = population_size
+        self.sigma = sigma
+        self.learning_rate = learning_rate
+
+    def _get_weight_from_population(self, weights, population):
+        weights_population = []
+        for index, i in enumerate(population):
+            jittered = self.sigma * i
+            weights_population.append(weights[index] + jittered)
+        return weights_population
+
+    def get_weights(self):
+        return self.weights
+
+    def train(self, epoch=100, print_every=1):
+        lasttime = time.time()
+        for i in range(epoch):
+            population = []
+            rewards = np.zeros(self.population_size)
+            for k in range(self.population_size):
+                x = []
+                for w in self.weights:
+                    x.append(np.random.randn(*w.shape))
+                population.append(x)
+            for k in range(self.population_size):
+                weights_population = self._get_weight_from_population(
+                    self.weights, population[k]
+                )
+                rewards[k] = self.reward_function(weights_population)
+            rewards = (rewards - np.mean(rewards)) / np.std(rewards)
+            for index, w in enumerate(self.weights):
+                A = np.array([p[index] for p in population])
+                self.weights[index] = (
+                        w
+                        + self.learning_rate
+                        / (self.population_size * self.sigma)
+                        * np.dot(A.T, rewards).T
+                )
+            if (i + 1) % print_every == 0:
+                print(
+                    'iter %d. reward: %f'
+                    % (i + 1, self.reward_function(self.weights))
+                )
+        print('time taken to train:', time.time() - lasttime, 'seconds')
+
+
+class Agent:
+
+    def __init__(self, model, money, max_buy, max_sell, close, skip, lr=0.03, sigma=0.1, population_size=15):
+        self.skip = skip
+        self.close = close
+        self.model = model
+        self.initial_money = money
+        self.window_size = model.input_size
+        self.max_buy = max_buy
+        self.max_sell = max_sell
+        self.es = Deep_Evolution_Strategy(
+            self.model.get_weights(),
+            self.get_reward,
+            population_size,
+            sigma,
+            lr
+        )
+
+    def act(self, sequence):
+        decision, buy = self.model.predict(np.array(sequence))
+        return np.argmax(decision[0]), int(buy[0])
+
+    def get_reward(self, weights):
+        initial_money = self.initial_money
+        starting_money = initial_money
+        len_close = len(self.close) - 1
+
+        self.model.weights = weights
+        state = get_state(self.close, 0, self.window_size + 1)
+        inventory = []
+        quantity = 0
+        for t in range(0, len_close, self.skip):
+            action, buy = self.act(state)
+            next_state = get_state(self.close, t + 1, self.window_size + 1)
+            if action == 1 and initial_money >= self.close[t]:
+                if buy < 0:
+                    buy = 1
+                if buy > self.max_buy:
+                    buy_units = self.max_buy
+                else:
+                    buy_units = buy
+                total_buy = buy_units * self.close[t]
+                initial_money -= total_buy
+                inventory.append(total_buy)
+                quantity += buy_units
+            elif action == 2 and len(inventory) > 0:
+                if quantity > self.max_sell:
+                    sell_units = self.max_sell
+                else:
+                    sell_units = quantity
+                quantity -= sell_units
+                total_sell = sell_units * self.close[t]
+                initial_money += total_sell
+
+            state = next_state
+        return ((initial_money - starting_money) / starting_money) * 100
+
+    def fit(self, iterations, checkpoint):
+        self.es.train(iterations, print_every=checkpoint)
+
+    def buy(self, dataset):
+        initial_money = self.initial_money
+        len_close = len(self.close) - 1
+        state = get_state(self.close, 0, self.window_size + 1)
+        starting_money = initial_money
+        states_sell = []
+        states_buy = []
+        inventory = []
+        quantity = 0
+        for t in range(0, len_close, self.skip):
+            action, buy = self.act(state)
+            next_state = get_state(self.close, t + 1, self.window_size + 1)
+            if action == 1 and initial_money >= self.close[t]:
+                if buy < 0:
+                    buy = 1
+                if buy > self.max_buy:
+                    buy_units = self.max_buy
+                else:
+                    buy_units = buy
+                total_buy = buy_units * self.close[t]
+                initial_money -= total_buy
+                inventory.append(total_buy)
+                quantity += buy_units
+                states_buy.append(t)
+                print(
+                    'day %d: buy %d units at price %f, total balance %f'
+                    % (t, buy_units, total_buy, initial_money)
+                )
+            elif action == 2 and len(inventory) > 0:
+                bought_price = inventory.pop(0)
+                if quantity > self.max_sell:
+                    sell_units = self.max_sell
+                else:
+                    sell_units = quantity
+                if sell_units < 1:
+                    continue
+                quantity -= sell_units
+                total_sell = sell_units * self.close[t]
+                initial_money += total_sell
+                states_sell.append(t)
+                try:
+                    invest = ((total_sell - bought_price) / bought_price) * 100
+                except:
+                    invest = 0
+                print(
+                    'day %d, sell %d units at price %f, investment %f %%, total balance %f,'
+                    % (t, sell_units, total_sell, invest, initial_money)
+                )
+            state = next_state
+
+        invest = ((initial_money - starting_money) / starting_money) * 100
+        print(
+            '\ntotal gained %f, total investment %f %%'
+            % (initial_money - starting_money, invest)
+        )
+        plt.figure(figsize=(20, 10))
+        plt.plot(dataset, label='true close', c='g')
+        plt.plot(
+            dataset, 'X', label='predict buy', markevery=states_buy, c='b'
+        )
+        plt.plot(
+            dataset, 'o', label='predict sell', markevery=states_sell, c='r'
+        )
+        plt.legend()
+        plt.show()
